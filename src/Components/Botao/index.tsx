8c404869bec2a735a86f598aafc0aeb37472c3c8
Enrico <enrico.leite@pca.com.br>
2025-04-19 18:17:28 -0300

    Utilização de props para informar uma mudança de estado; Utilizamos a junção de props + state para podermos passar state para outros componentes e para podermos avisar que um estado deve ser mudado.

--- src/Components/Botao/index.tsx
+++ src/Components/Botao/index.tsx
@@ -3,12 +3,14 @@ import style from './Botao.module.scss';
 
 interface BotaoProps {
   children: React.ReactNode;
+  type?: "button" | "submit" | "reset";
 }
 
 class Botao extends React.Component<BotaoProps> {
   render() {
+    const { type = "button" } = this.props;
     return (
-      <button className={style.botao}>
+      <button type={type} className={style.botao}>
         {this.props.children}
       </button>
     );
